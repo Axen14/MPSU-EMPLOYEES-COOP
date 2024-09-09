f7501e1c6e57f6034c55c68346e1c49c76014bd9
unknown <bernardezmarbert972@gmail.com>
2024-09-09 22:04:48 +0800

    under member loans and payment

---
+++ mpsu_coop_front/src/member/Home/Loans/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Loans from './admin/Loans/Loans';
+
+function App() {
+  return (
+    <div className="App">
+      <Loans />
+    </div>
+  );
+}
+
+export default App;
---
+++ mpsu_coop_front/src/member/Home/Payments/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Payments from './admin/Payments/Payments';
+
+function App() {
+  return (
+    <div className="App">
+      <Payments />
+    </div>
+  );
+}
+
+export default App;
