2c293ff15aacb9c05585e59be5b888198de77967
Allen Fabag <glitzlhen124@gmail.com>
2024-11-25 13:05:57 +0800

    Fixed the Print eme

---
+++ mcoop_app/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    MemberViewSet, 
+    AccountViewSet, 
+    LoanViewSet,
+    PaymentScheduleViewSet,
+    PaymentViewSet,
+    ActiveLoansByAccountView
+)
+
+router = DefaultRouter()
+router.register(r'members', MemberViewSet)  
+router.register(r'accounts', AccountViewSet)
+router.register(r'loans', LoanViewSet) 
+router.register(r'payment-schedules', PaymentScheduleViewSet, basename='payment-schedules')
+router.register(r'payments', PaymentViewSet, basename='payment')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    
+    
+
+    
+]
